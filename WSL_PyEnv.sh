482db25dd2796581872b245c43504cecabe92219
shichao <jiva99@gmail.com>
2019-03-07 15:20:08 -0800

    fix bash source issue and add ipython profile part

--- WSL_PyEnv.sh
+++ WSL_PyEnv.sh
@@ -1,27 +1,38 @@
+#!/bin/bash -i
+# Update apt
 sudo apt-get update
+
 # Install required packages and tools
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
 # Install pyenv
 curl https://pyenv.run | bash
 echo '# pyenv' >> ~/.bashrc
 echo "export PATH=\"${HOME}/.pyenv/bin:\$PATH\"" >> ~/.bashrc
 echo "eval \"\$(pyenv init -)\"" >> ~/.bashrc
 echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bashrc
+source ~/.bashrc
+
+# Install pyenv virtual env wrapper
 git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(pyenv root)/plugins/pyenv-virtualenvwrapper
+echo 'pyenv virtualenvwrapper_lazy' >> ~/.bashrc
 source ~/.bashrc
 
 # Install python 3
 pyenv install 3.7.2
 pyenv global 3.7.2
+
 # Update pip
 pip install --upgrade pip
+
 # Install pipenv
 pip install --upgrade pipenv
 echo '# pipenv' >> ~/.bashrc
 echo 'eval "$(pipenv --completion)"' >> ~/.bashrc
-source ~/.bashrcex
+source ~/.bashrc
+
 # Create env for jupyter3
 pyenv virtualenv 3.7.2 jupyter3
 export PYENV_VERSION=jupyter3
@@ -31,3 +42,8 @@ python -m ipykernel install --user
 pip install ipywidgets
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 unset PYENV_VERSION
+
+# Ipython profile
+pyenv global 3.7.2 jupyter3
+ipython profile create
+curl -L http://hbn.link/hb-ipython-startup-script > ~/.ipython/profile_default/startup/00-venv-sitepackages.py
