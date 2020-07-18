869dce84ae55192ee93d71b1a8ba06a054b3e6ad
Chao Shi <chao.shi.ca@gmail.com>
2020-07-18 00:10:42 -0700

    add mc

--- WSL_U20.4_PyEnv.sh
+++ WSL_U20.4_PyEnv.sh
@@ -12,7 +12,7 @@ sudo apt update
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python-openssl \
-git python3-pip pt-transport-https \
+git python3-pip pt-transport-https mc \
 # dotnet-sdk-2.1 dotnet-sdk-3.1
 
 # Install pyenv
