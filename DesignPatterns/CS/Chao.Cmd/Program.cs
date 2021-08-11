b0744d7ca5108657fcd9de81fb2a83becc2d547f
Chao Shi <cshi@bgcengineering.ca>
2021-08-11 13:54:01 -0700

    --

--- DesignPatterns/CS/Chao.Cmd/Program.cs
+++ DesignPatterns/CS/Chao.Cmd/Program.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using Chao.DesignPatterns.Creational;
 
 namespace Chao.Cmd
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Design Patterns!");
-            Console.WriteLine("== Creational Patterns ==");
-
-            //2. Prototype
-            var prototype = new Prototype();
-            prototype.Run();
-        }
+      int a = 3;
+      Console.WriteLine((4 & 1) != 0);
     }
+
+  }
 }
